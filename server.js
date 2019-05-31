1f2b9598f7f3ec5629751a075167ab38a3fe8b13
Nsix6 <nsikakmbre@gmail.com>
2019-05-31 10:08:37 +0100

    'first'

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const app = express();
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "test",
+    database: "smart_brain"
+  }
+});
+
+// bodyParser middleware, so we can have access to json data from front-end
+// in this case to access the body
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+// SIGNIN ROUTE
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+// Register Route
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApicall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
+
+bcrypt.hash("bacon", null, null, function(err, hash) {
+  // Store hash in your password DB.
+});
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
